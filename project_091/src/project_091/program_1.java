bb4429094c2ef00e8b1c3f440d1ea074eb35ceae
sainib <sainib@LIN-F4NX9K3.corp.capgemini.com>
2024-03-04 10:04:18 +0530

    this is my 1st commit

---
+++ project_091/src/project_091/program_1.java
@@ -0,0 +1,19 @@
+package project_091;
+
+public class program_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = new String();
+		System.out.println(s);
+		String s1 = new String("java");
+		System.out.println(s1);
+		
+		char ch [] = {'j','a','v','a'};
+		String s2 = new String(ch);
+		System.out.println(s2);
+
+	}
+
+}
+
